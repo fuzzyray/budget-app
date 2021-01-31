e3e8dd3e48fd6e800fae66218aabfe3bba839417
Paul Varner <fuzzyray@gmail.com>
2021-01-31 11:25:56 -0600

    Implement required instance variables and empty class methods

--- budget.py
+++ budget.py
@@ -1,7 +1,24 @@
 class Category:
+    def __init__(self, category):
+        self.category = category
+        self.ledger = []
+        self.__balance = 0
 
+    def deposit(self, amount, description=""):
+        pass
 
+    def withdraw(self, amount, description=""):
+        pass
 
+    def get_balance(self):
+        pass
+
+    def transfer(self, amount, category_instance):
+        pass
+
+    def check_funds(self, amount):
+        pass
 
 
 def create_spend_chart(categories):
+    pass
