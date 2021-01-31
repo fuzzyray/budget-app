3aedcba7661a9d6ce16eda03f93158f302dce9ed
Paul Varner <fuzzyray@gmail.com>
2021-01-31 12:45:44 -0600

    Fully implement budget.Category class

--- budget.py
+++ budget.py
@@ -2,23 +2,44 @@ class Category:
     def __init__(self, category):
         self.category = category
         self.ledger = []
-        self.__balance = 0
+        self.__balance = 0.0
+
+    def __repr__(self):
+        header = self.category.center(30, "*") + "\n"
+        ledger = ""
+        for item in self.ledger:
+            ledger += "{:<23}{:>7.2f}\n".format(item["description"][:23], item["amount"])
+        total = "Total: {:.2f}".format(self.__balance)
+        return "{}{}{}".format(header, ledger, total)
 
     def deposit(self, amount, description=""):
-        pass
+        self.ledger.append({"amount": amount, "description": description})
+        self.__balance += amount
 
     def withdraw(self, amount, description=""):
-        pass
+        if self.__balance - amount >= 0:
+            self.ledger.append({"amount": -1 * amount, "description": description})
+            self.__balance -= amount
+            return True
+        else:
+            return False
 
     def get_balance(self):
-        pass
+        return self.__balance
 
     def transfer(self, amount, category_instance):
-        pass
+        if self.withdraw(amount, "Transfer to {}".format(category_instance.category)):
+            category_instance.deposit(amount, "Transfer from {}".format(self.category))
+            return True
+        else:
+            return False
 
     def check_funds(self, amount):
-        pass
+        if self.__balance >= amount:
+            return True
+        else:
+            return False
 
 
 def create_spend_chart(categories):
-    pass
+    return "spend_chart"
