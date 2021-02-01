11a16c4019fdbf6dbe5e133e9ce4ee027dad0541
Paul Varner <fuzzyray@gmail.com>
2021-01-31 18:37:59 -0600

    Implement function create_spend_chart()

--- budget.py
+++ budget.py
@@ -46,4 +46,37 @@ class Category:
 
 
 def create_spend_chart(categories):
-    return "spend_chart"
+    spent_amounts = []
+    # Get total spent in each category
+    for category in categories:
+        spent = 0
+        for item in category.ledger:
+            if item["amount"] < 0:
+                spent += abs(item["amount"])
+        spent_amounts.append(round(spent, 2))
+
+    # Calculate percentage rounded down to the nearest 10
+    total = round(sum(spent_amounts), 2)
+    spent_percentage = list(map(lambda amount: int((((amount / total) * 10) // 1) * 10), spent_amounts))
+
+    # Create the bar chart substrings
+    header = "Percentage spent by category\n"
+
+    chart = ""
+    for value in reversed(range(0, 101, 10)):
+        chart += str(value).rjust(3) + '|'
+        for percent in spent_percentage:
+            if percent >= value:
+                chart += " o "
+            else:
+                chart += "   "
+        chart += " \n"
+
+    footer = "    " + "-" * ((3 * len(categories)) + 1) + "\n"
+    descriptions = list(map(lambda category: category.description, categories))
+    max_length = max(map(lambda description: len(description), descriptions))
+    descriptions = list(map(lambda description: description.ljust(max_length), descriptions))
+    for x in zip(*descriptions):
+        footer += "    " + "".join(map(lambda s: s.center(3), x)) + " \n"
+
+    return (header + chart + footer).rstrip("\n")
